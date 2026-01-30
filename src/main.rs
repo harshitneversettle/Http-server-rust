ba8430a68efff5cc7688fc7ce369463702153ee6
Harshit yadav <harshityadav5499@gmail.com>
2026-01-30 06:49:18 +0530

    Add: server is responding with body V0

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").expect("Could not bind to port 3000");
-    println!("TCP server listening");
+    // println!("TCP server listening");
 
     for stream in listener.incoming() {
         match stream {
@@ -19,22 +19,31 @@ fn main() {
         }
     }
 }
-
 fn handle_client(mut stream: TcpStream) {
     // loop => without loop , i can only send 1 message from client to server , breaks on pressing space , with loop i can keep sending messages
 
     let mut buffer = [0u8; 512]; // buffer is just a data/memory for storing the bytes received 
-    let _n = stream.read(&mut buffer).unwrap(); // read returns how many bytes i just read 
-    let client_message = String::from_utf8_lossy(&buffer); // utf is basically for converting u8 assay to string (at a high level)
+    let n = stream.read(&mut buffer).unwrap(); // read returns how many bytes i just read 
+    let client_message = String::from_utf8_lossy(&buffer[..n]); // utf is basically for converting u8 assay to string (at a high level)
     let split_message : Vec<&str> = client_message.split("\r\n").collect() ;
     let request_line = split_message[0] ;
-    let url_path : Vec<&str> = request_line.split(" ").collect() ;
-    // println!("{:?}" , url_path[1]) ;
+    let req_line_split : Vec<&str> = request_line.split(" ").collect() ;
+    let url_path = req_line_split[1] ;
+    // println!("{:?}" , url_path) ;
     let response ;
-    if url_path[1].len() >= 2 {
-        response = "HTTP/1.1 404 Not Found\r\n\r\n" ;
-    }else {
-        response = "HTTP/1.1 200 OK\r\n\r\n" ;
+    match url_path {
+        _ if url_path.starts_with("/echo") => {
+            let body = url_path.trim_start_matches("/echo/") ;
+            let body_len = body.len() ;
+            // println!("{}" , body) ;
+            response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}" , body_len , body) ;
+        }
+        "/" => {
+            response = "HTTP/1.1 200 OK\r\n\r\n".to_string();
+        }
+        _ => {
+            response = "HTTP/1.1 404 Not Found\r\n\r\n".to_string() ;
+        }
     }
 
     stream.write(response.as_bytes()).unwrap() ;
