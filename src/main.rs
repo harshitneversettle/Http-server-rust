c4234a913d1941deb7e1ab5a5d8b112d6b071462
Harshit yadav <harshityadav5499@gmail.com>
2026-02-01 03:59:10 +0530

    Addd: returning a file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+use std::{env, fs, path::Path};
+#[allow(unused_variables)]
 use std::{
     io::{Read, Write},
-    net::{TcpListener, TcpStream}, thread,
+    net::{TcpListener, TcpStream},
+    thread,
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").expect("Could not bind to port 3000");
+    let listener = TcpListener::bind("127.0.0.1:3000").expect("Could not bind to port 3000");
     // println!("TCP server listening");
-
     for stream in listener.incoming() {
         match stream {
-            Ok(_) => {
+            Ok(stream) => {
                 let _handle = thread::spawn(|| {
                     println!("Client connected!");
-                    handle_client(stream.unwrap());
-                }) ;
+                    handle_client(stream);
+                });
             }
             Err(e) => {
                 println!("Connection failed: {}", e);
@@ -23,7 +25,6 @@ fn main() {
 }
 fn handle_client(mut stream: TcpStream) {
     // loop => without loop , i can only send 1 message from client to server , breaks on pressing space , with loop i can keep sending messages
-
     let mut buffer = [0u8; 512]; // buffer is just a data/memory for storing the bytes received 
     let n = stream.read(&mut buffer).unwrap(); // read returns how many bytes i just read 
     let client_message = String::from_utf8_lossy(&buffer[..n]); // utf is basically for converting u8 assay to string (at a high level)
@@ -33,7 +34,8 @@ fn handle_client(mut stream: TcpStream) {
     let headers = &split_message[1..];
     let req_line_split: Vec<&str> = request_line.split(" ").collect();
     let url_path = req_line_split[1];
-    let mut response;
+    let mut response = String::new();
+    // println!("{}" , url_path) ;
 
     match url_path {
         _ if url_path.starts_with("/echo") => {
@@ -44,9 +46,42 @@ fn handle_client(mut stream: TcpStream) {
                 body_len, body
             );
         }
+        _ if url_path.starts_with("/files") => {
+            let dir: Vec<String> = env::args().collect();
+            let dir_path = dir[2].to_string();
+            println!("{:?}", dir);
+            let path = url_path.trim_start_matches("/files/").to_string();
+            let full_path = Path::new(&dir_path).join(path);
+            println!("okok1");
+            if full_path.exists() {
+                println!("{}", full_path.display());
+                let read_file = match fs::read(&full_path) {
+                    Result::Ok(s) => s,
+                    Result::Err(e) => {
+                        println!("{} okok", e);
+                        return;
+                    }
+                };
+                let read_string = match String::from_utf8(read_file) {
+                    Result::Ok(s) => s,
+                    Result::Err(e) => {
+                        println!("{}", e);
+                        return;
+                    }
+                };
+                println!("{}", read_string);
+                let read_string_len = read_string.len();
+                response = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                    read_string_len, read_string
+                );
+            }
+        }
+
         "/" => {
             response = "HTTP/1.1 200 OK\r\n\r\n".to_string();
         }
+
         _ => {
             response = "HTTP/1.1 404 Not Found\r\n\r\n".to_string();
         }
@@ -63,4 +98,3 @@ fn handle_client(mut stream: TcpStream) {
     }
     stream.write(response.as_bytes()).unwrap();
 }
-
